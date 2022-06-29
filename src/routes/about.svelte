68c692793a40b04e695bfed3f7bbd27e639fcf08
natcel <natcelnieves@gmail.com>
2022-06-29 19:42:13 -0400

    Modified about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,4 @@
-
 <div class="text-center">
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
-<p>Web app for encrypting text.</p>
+<p>Web app for encrypting text with various other encryptions(AES, DES, Base64, RC4) to encrypt and decrypt.</p>
 </div>
